225ec3bc2a4d450378996a5b257d8e495eeca7f7
Tamsen1995 <Tamanhbui95@gmail.com>
2017-11-20 17:34:41 -0800

    [UPD] I can now successfully make a twitter search request, however I would very much like to be able to modularize the search and the token request. So that the search will actually wait for the token to be finished

--- tweet_showing_app/APIController.swift
+++ tweet_showing_app/APIController.swift
@@ -25,7 +25,9 @@ class APIController {
     func SearchRequest(contains: String) {
         
         // make a request using the unique bearer token here
-        guard let url = URL(string: "https://api.twitter.com/1.1/search/tweets.json") else {
+        
+        let q = contains.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        guard let url = URL(string: "https://api.twitter.com/1.1/search/tweets.json?q=\(q!))&count=100&lang=fr&result_type=recent") else {
             print("ERROR: url was invalid")
             return
         }
@@ -35,13 +37,9 @@ class APIController {
         if let token = self.token {
             request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         }
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+     //   request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: contains, options: []) else {
-            print("Couldn't make httpBody")
-            return
-        }
-        request.httpBody = httpBody
+
         
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
@@ -61,47 +59,7 @@ class APIController {
     }
     
     // MARK: The request towards twitter in order to get an OAuth2.0 token
-    
-    func getToken() {
-        // Make POST request to the oauth2 endpoint to get the token
-        
-        //authorization
-        let CONSUMER_KEY = "NeH3OHvCf3bpVSdV2flF8VNOp"
-        let CONSUMER_SECRET = "YWsC8y6S7ocAq8HIx21ndKppdFPW6fq6s5ubw2SlEy4lztC4QX"
-        let BEARER = ((("\(CONSUMER_KEY):\(CONSUMER_SECRET)")).base64Encoded())!
-        
-        guard let url = URL(string: "https://api.twitter.com/oauth2/token") else {
-            print("ERROR: The token url was invalid")
-            return
-        }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("Basic \(BEARER)", forHTTPHeaderField: "Authorization")
-        request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        request.httpBody = "grant_type=client_credentials".data(using: String.Encoding.utf8)
-        let session = URLSession.shared
-        
-        session.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print("Response:\n\n \(response)\n\n\n")
-            }
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    // extract the bearer token from here and set it to self.token
-                    print("The json received is the following:\n \(json)\n\n\n")
-                    if let dictionary = json as? [String: String] {
-                        if let token = dictionary["access_token"] {
-                            print("The bearer token received is: \(token)\n\n")
-                            self.token = token
-                        }
-                    }
-                } catch {
-                    print(error)
-                }
-            }
-            }.resume()
-    }
+
     
 }
 
--- tweet_showing_app/ViewController.swift
+++ tweet_showing_app/ViewController.swift
@@ -9,24 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController, APITwitterDelegate {
-
+    
     // TODO figure out how to make this work
-
+    
+    var token : String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let controller = APIController(delegate: self, token: "NeH3OHvCf3bpVSdV2flF8VNOp")
-        controller.getToken()
+        getToken()
+
+        
         
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func Tweet(tweet: [Tweet]) {
         print(tweet)
     }
@@ -34,6 +40,57 @@ class ViewController: UIViewController, APITwitterDelegate {
     func ErrorCase(error: NSError) {
         print(error)
     }
-
+    
+    
+    func getToken() {
+        // Make POST request to the oauth2 endpoint to get the token
+        
+        //authorization
+        let CONSUMER_KEY = "NeH3OHvCf3bpVSdV2flF8VNOp"
+        let CONSUMER_SECRET = "YWsC8y6S7ocAq8HIx21ndKppdFPW6fq6s5ubw2SlEy4lztC4QX"
+        let BEARER = ((("\(CONSUMER_KEY):\(CONSUMER_SECRET)")).base64Encoded())!
+        
+        guard let url = URL(string: "https://api.twitter.com/oauth2/token") else {
+            print("ERROR: The token url was invalid")
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Basic \(BEARER)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "grant_type=client_credentials".data(using: String.Encoding.utf8)
+        let session = URLSession.shared
+        
+        let queue = DispatchQueue(label: "TokenGetter")
+        queue.sync {
+            session.dataTask(with: request) { (data, response, error) in
+                if let response = response {
+                    print("Response:\n\n \(response)\n\n\n")
+                }
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: [])
+                        // extract the bearer token from here and set it to self.token
+                        print("The json received is the following:\n \(json)\n\n\n")
+                        if let dictionary = json as? [String: String] {
+                            if let token = dictionary["access_token"] {
+                                print("The bearer token received is: \(token)\n\n")
+                                self.token = token as String // might be redundant to cast
+                                
+                                if let token = self.token {
+                                    let controller = APIController(delegate: self, token: token)
+                                    controller.SearchRequest(contains: "whatever")
+                                }
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+                }.resume()
+        }
+        print(self.token)
+    }
+    
 }
 
